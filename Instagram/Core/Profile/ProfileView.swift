db66a9a1b625addd0f858b32000c7f23c9eb76ab
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-11-26 12:13:01 +0100

    Implement profile view

---
+++ Instagram/Core/Profile/ProfileView.swift
@@ -0,0 +1,104 @@
+//
+//  ProfileView.swift
+//  Instagram
+//
+//  Created by Khawla Zarrami on 26/11/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    let gridItems: [GridItem] = [
+        .init( .flexible(), spacing: 1),
+        .init( .flexible(), spacing: 1),
+        .init( .flexible(), spacing: 1)
+    ]
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                // header
+                
+                VStack(spacing: 10) {
+                    
+                    // pic and stats
+                    HStack {
+                        Image("profile-2")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height:80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
+                            UserStatView(value: 12, title: "Followers")
+                            UserStatView(value: 24, title: "Following")
+                        }
+                        
+                        
+                    }
+                    .padding(.horizontal)
+                    
+                    
+                    // name and bio
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Chadwick Bozeman")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("Wakanda Forever")
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    // action button
+                    
+                    Button {
+                        
+                    }label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundStyle(.black)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.black, lineWidth: 1)
+                            }
+                    }
+                    // Divider
+                    Divider()
+                }
+                
+                // post grid view
+                
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0 ... 15 , id : \.self) { index in
+                        Image("profile-1")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                }
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundStyle(.black)
+                    }
+                }
+            }
+        }
+}
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ Instagram/Core/Tabbar/MainTabView.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabView.swift
+//  Instagram
+//
+//  Created by Khawla Zarrami on 26/11/2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+            }
+            
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
