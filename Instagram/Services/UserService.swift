4163ee09525d43b9fba5c9ea66867897f4669db9
Khawla Zarrami <zarramikhaoula@gmail.com>
2026-01-07 16:51:51 +0100

    Fetch all users

---
+++ Instagram/Services/UserService.swift
@@ -0,0 +1,20 @@
+//
+//  UserService.swift
+//  Instagram
+//
+//  Created by Khawla Zarrami on 07/01/2026.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+struct UserService {
+    
+    static func fetchAllUsers() async throws -> [User] {
+      
+        let snapshot = try await Firestore.firestore().collection("users").getDocuments()
+        
+        return snapshot.documents.compactMap({ try? $0.data(as: User.self)})
+    }
+}
